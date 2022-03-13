a26406323bc972c4f2e7ad83ece557d89dda9b5e
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2022-03-13 16:19:52 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mySecrets',
-    version='1.0.3',
+    version='1.0.4',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/mySecrets',
@@ -10,25 +10,6 @@ setup(
     packages=['mySecrets'],
     python_requires='>=3',
     platforms=["all"],
-    license='GPL-2.0 License',
-    entry_points={
-        'console_scripts': [
-            'getHash=mySecrets:getHash',
-            'encrypt=mySecrets:encrypt',
-            'decrypt=mySecrets:decrypt',
-            'hexToStr=mySecrets:hexToStr',
-            'toHex=mySecrets:toHex',
-            'hexToJtc64=mySecrets:hexToJtc64',
-            'strToJtc64=mySecrets:strToJtc64',
-            'jtc64ToHex=mySecrets:jtc64ToHex',
-            'jtc64ToStr=mySecrets:jtc64ToStr',
-            'NotHexError=mySecrets:NotHexError',
-            'NotJtc64Error=mySecrets:NotJtc64Error',
-            'InputError=mySecrets:InputError',
-            'InvalidCiphertextError=mySecrets:InvalidCiphertextError',
-            'VersionNotSupportError=mySecrets:VersionNotSupportError',
-            'PasswordWrongError=mySecrets:PasswordWrongError'
-        ]
-    }
+    license='GPL-2.0 License'
 )
 
